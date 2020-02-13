fb7fdf869b14b13536b60e9c5af06adba0bec94a
kaiwen0627 <kaiwen0425@gmail.com>
2020-02-13 11:03:10 +0800

    change themes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,17 +2,19 @@ module.exports = {
   title: '小小错题本',
   description: '开发遇到的问题',
   base: '',
-  themeConfig: {
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'vue相关', link: '/vue/' },
-      { text: 'External', link: 'https://google.com' }
-    ],
-    sidebar: [
-      ['/vue/', 'Vue相关'],
-      ['/react/', 'React相关'],
-      ['/js/', 'js相关'],
-      ['/other/', '杂谈'],
-    ]
-  }
+  theme: '@vuepress/theme-blog',
+  // themeConfig: {
+  //   nav: [
+  //     { text: 'Home', link: '/' },
+  //     { text: 'vue相关', link: '/vue/' },
+  //     { text: 'External', link: 'https://google.com' }
+  //   ],
+  //   sidebar: [
+  //     ['/vue/', 'Vue相关'],
+  //     ['/react/', 'React相关'],
+  //     ['/js/', 'js相关'],
+  //     ['/other/', '杂谈']
+  //   ]
+  // },
+  plugins: ['@vuepress/back-to-top']
 };
